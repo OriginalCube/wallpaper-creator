c3b6ec6fd13a3618310358720e3f5f6e882090fe
OriginalCube <lancemiraflorcube@gmail.com>
2023-11-27 20:34:03 +0800

    improvement: icon

--- components/wallpaper/Player.vue
+++ components/wallpaper/Player.vue
@@ -1,32 +1,12 @@
 <template>
 	<div class="mt-4 w-2/5 h-24 flex flex-col items-center justify-center">
 		<input class="w-11/12" type="range" />
-		<div class="flex items-center justify-evenly">
-			<UButton
-				icon="i-mdi-refresh"
-				class="bg-transparent hover:bg-transparent"
-				size="xl"
-			/>
-			<UButton
-				icon="i-mdi-rewind"
-				class="bg-transparent hover:bg-transparent"
-				size="xl"
-			/>
-			<UButton
-				icon="i-mdi-pause"
-				class="bg-transparent hover:bg-transparent"
-				size="xl"
-			/>
-			<UButton
-				icon="i-mdi-fast-forward"
-				class="bg-transparent hover:bg-transparent"
-				size="xl"
-			/>
-			<UButton
-				icon="i-mdi-shuffle"
-				class="bg-transparent hover:bg-transparent"
-				size="xl"
-			/>
+		<div class="flex items-center gap-12 justify-evenly mt-6">
+			<img src="/assets/images/replay.png" class="w-6" />
+			<img src="/assets/images/backward.png" class="w-6" />
+			<img src="/assets/images/pause.png" class="w-8" />
+			<img src="/assets/images/forward.png" class="w-6" />
+			<img src="/assets/images/shuffle.png" class="w-6" />
 		</div>
 	</div>
 </template>
